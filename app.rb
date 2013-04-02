22abc0cadba7cfc5f00935e830e7ebf7791aa419
rkononov <rkononov@gmail.com>
2013-04-02 16:14:51 +0600

    trying to workaround public_folder

--- app.rb
+++ app.rb
@@ -1,52 +1,60 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'faker'
 require 'iron_worker_ng'
 require 'iron_cache'
 require_relative 'helpers/iron_cache_storage.rb'
-
-configure do
-  set :public_folder, Proc.new { File.join(root, "static") }
+my_app = Sinatra.new do
+  #set :public_folder, './static' #doesn't work in IronPaas
   set :storage, IronCacheStorage.new
   set :worker, IronWorkerNG::Client.new
   set :port, port = ENV['PORT'] ? ENV['PORT'].to_i : 4567
-end
+  enable :logging, :dump_errors, :raise_errors
 
-get '/' do
-  @caches = settings.storage.get_caches.select{|c| c=~ /^\d+/}
-  @saved = settings.storage.get_from_cache('saved_search')
-  erb "Hey"
-end
-
-post '/store_question' do
-  if params["q"]
-    saved = settings.storage.get_from_cache('saved_search')
-    puts "Number of questions:#{saved.count}"
-    saved << params["q"]
-    settings.storage.put_to_cache(saved,'saved_search')
+  #workaround to debug public folder
+  get '/static/*' do
+    puts "requested file:#{params[:splat]}"
+    send_file File.join('static', params[:splat])
   end
-  puts "putting question to cache:#{params.inspect}"
-end
 
-post '/remove_question' do
-  if params["q"]
-    saved = settings.storage.get_from_cache('saved_search')
-    puts "Number of questions:#{saved.count}"
-    saved.delete(params["q"])
-    settings.storage.put_to_cache(saved,'saved_search')
+  get '/' do
+    puts "Hey hey hey!"
+    @caches = settings.storage.get_caches.select{|c| c=~ /^\d+/}
+    @saved = settings.storage.get_from_cache('saved_search')
+    erb "Hey"
   end
-  puts "remove question from cache :#{params.inspect}"
-end
 
-get '/cache_results' do
-  params["cache"] ? settings.storage.get_from_cache(params["cache"]).to_json : [].to_json
-end
+  post '/store_question' do
+    if params["q"]
+      saved = settings.storage.get_from_cache('saved_search')
+      puts "Number of questions:#{saved.count}"
+      saved << params["q"]
+      settings.storage.put_to_cache(saved,'saved_search')
+    end
+    puts "putting question to cache:#{params.inspect}"
+  end
 
-get '/cache_details' do
-  params["cache"] ? settings.storage.get_value_from_cache('keywords', params["cache"]).to_json : ''
-end
+  post '/remove_question' do
+    if params["q"]
+      saved = settings.storage.get_from_cache('saved_search')
+      puts "Number of questions:#{saved.count}"
+      saved.delete(params["q"])
+      settings.storage.put_to_cache(saved,'saved_search')
+    end
+    puts "remove question from cache :#{params.inspect}"
+  end
 
-post '/queue_worker' do
-  puts "PARAMS:#{params.inspect}"
-  keywords = params["keywords"] ? params["keywords"].split(',') : ['rabbitmq', 'python+celery', 'message queue', 'python+workers', 'python+background']
-  settings.worker.tasks.create("CustomSearchReport", {"keywords"=>keywords})
+  get '/cache_results' do
+    params["cache"] ? settings.storage.get_from_cache(params["cache"]).to_json : [].to_json
+  end
+
+  get '/cache_details' do
+    params["cache"] ? settings.storage.get_value_from_cache('keywords', params["cache"]).to_json : ''
+  end
+
+  post '/queue_worker' do
+    puts "PARAMS:#{params.inspect}"
+    keywords = params["keywords"] ? params["keywords"].split(',') : ['rabbitmq', 'python+celery', 'message queue', 'python+workers', 'python+background']
+    settings.worker.tasks.create("CustomSearchReport", {"keywords"=>keywords})
+  end
 end
+my_app.run!
